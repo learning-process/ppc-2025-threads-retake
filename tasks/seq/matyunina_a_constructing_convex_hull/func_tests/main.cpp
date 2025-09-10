d069c2271e4057d744d2d9e4186dfa378e9fbac2
erty1909 <ERTY-00@yandex.ru>
2025-09-10 16:10:19 +0300

    test openCV

--- tasks/seq/matyunina_a_constructing_convex_hull/func_tests/main.cpp
+++ tasks/seq/matyunina_a_constructing_convex_hull/func_tests/main.cpp
@@ -6,6 +6,9 @@
 #include <memory>
 #include <string>
 #include <vector>
+#ifndef _WIN32
+#include <opencv2/opencv.hpp>
+#endif
 
 #include "core/task/include/task.hpp"
 #include "core/util/include/util.hpp"
@@ -38,7 +41,63 @@ void RunTest(int height, int width, std::vector<int> image,
 
   EXPECT_EQ(points, ans);
 }
+
+#ifndef _WIN32
+bool RunImageTest(std::string image_path, std::string ans_path) {
+  cv::Mat image = cv::imread(image_path);
+  if (image.empty()) {
+    return false;
+  }
+  cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
+  cv::threshold(image, image, 128, 1, cv::THRESH_BINARY);
+
+  std::vector<int> pixels(image.rows * image.cols);
+  for (int i = 0; i < image.rows; i++) {
+    for (int j = 0; j < image.cols; j++) {
+      pixels[(i * image.cols + j)] = image.at<uchar>(i,j);
+    }
+  }
+
+  int width = image.cols;
+  int height = image.rows;
+
+  auto task_data = std::make_shared<ppc::core::TaskData>();
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(pixels.data()));
+  task_data->inputs_count.emplace_back(width);
+  task_data->inputs_count.emplace_back(height);
+
+  matyunina_a_constructing_convex_hull_seq::ConstructingConvexHull constructingConvexHull(task_data);
+  constructingConvexHull.Validation();
+  constructingConvexHull.PreProcessing();
+  constructingConvexHull.Run();
+  constructingConvexHull.PostProcessing();
+
+  matyunina_a_constructing_convex_hull_seq::Point* pointArray =
+      reinterpret_cast<matyunina_a_constructing_convex_hull_seq::Point*>(task_data->outputs[0]);
+  std::vector<matyunina_a_constructing_convex_hull_seq::Point> points(pointArray,
+                                                                      pointArray + task_data->outputs_count[0]);
+
+  std::cout << "\n#######\n";
+  for (matyunina_a_constructing_convex_hull_seq::Point& point: points) {
+    std::cout<< "x: " << point.x << " y: " << point.y << "\n";
+  }
+  std::cout << "\n#######\n";
+
+  image.release();
+
+  return true;
+}
+#endif
 }  // namespace
+
+#ifndef _WIN32
+TEST(matyunina_a_constructing_convex_hull_seq, image1) {
+  std::string src_path = ppc::util::GetAbsolutePath("seq/matyunina_a_constructing_convex_hull_seq/data/image1.png");
+  std::string exp_path = ppc::util::GetAbsolutePath("seq/matyunina_a_constructing_convex_hull_seq/data/0_expected.png");
+
+  ASSERT_TRUE(RunImageTest(src_path, exp_path));
+}
+#endif
 
 TEST(matyunina_a_constructing_convex_hull_seq, test_9_9) {
   int height = 9;
