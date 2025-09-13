049fe8346a53c30f4f5e6772138b81e0d994334f
NeiroYT <neiro15000000let@gmail.com>
2025-09-13 15:41:00 +0300

    Clang...

--- tasks/seq/leontev_n_graham/func_tests/main.cpp
+++ tasks/seq/leontev_n_graham/func_tests/main.cpp
@@ -2,14 +2,11 @@
 
 #include <cstddef>
 #include <cstdint>
-#include <fstream>
 #include <memory>
-#include <string>
-#include <vector>
 #include <random>
+#include <vector>
 
 #include "core/task/include/task.hpp"
-#include "core/util/include/util.hpp"
 #include "seq/leontev_n_graham/include/ops_seq.hpp"
 
 namespace {
@@ -29,23 +26,23 @@ TEST(leontev_n_graham_seq, test_10_points_basic) {
   constexpr size_t kCount = 10;
 
   // Create data
-  std::vector<float> in_X = {0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f};
-  std::vector<float> in_Y = {0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 8.0f};
-  std::vector<float> out_X(kCount, 0.0f);
-  std::vector<float> out_Y(kCount, 0.0f);
+  std::vector<float> in_x = {0.0F, 1.0F, 2.0F, 3.0F, 4.0F, 5.0F, 6.0F, 7.0F, 8.0F, 9.0F};
+  std::vector<float> in_y = {0.0F, 1.0F, 2.0F, 3.0F, 4.0F, 5.0F, 6.0F, 7.0F, 8.0F, 8.0F};
+  std::vector<float> out_x(kCount, 0.0F);
+  std::vector<float> out_y(kCount, 0.0F);
   int out_size = 0;
-  std::vector<float> true_result_X = {0.0f, 9.0f, 8.0f};
-  std::vector<float> true_result_Y = {0.0f, 8.0f, 8.0f};
+  std::vector<float> true_result_x = {0.0F, 9.0F, 8.0F};
+  std::vector<float> true_result_y = {0.0F, 8.0F, 8.0F};
 
   // Create task_data
   auto task_data_seq = std::make_shared<ppc::core::TaskData>();
-  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in_X.data()));
-  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in_Y.data()));
-  task_data_seq->inputs_count.emplace_back(in_X.size());
-  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_X.data()));
-  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_Y.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in_x.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in_y.data()));
+  task_data_seq->inputs_count.emplace_back(in_x.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_x.data()));
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_y.data()));
   task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(&out_size));
-  task_data_seq->outputs_count.emplace_back(out_X.size());
+  task_data_seq->outputs_count.emplace_back(out_x.size());
 
   // Create Task
   leontev_n_graham_seq::GrahamSeq test_task_sequential(task_data_seq);
@@ -53,41 +50,41 @@ TEST(leontev_n_graham_seq, test_10_points_basic) {
   test_task_sequential.PreProcessing();
   test_task_sequential.Run();
   test_task_sequential.PostProcessing();
-  out_X.resize(out_size);
-  out_Y.resize(out_size);
-  EXPECT_EQ(out_X, true_result_X);
-  EXPECT_EQ(out_Y, true_result_Y);
+  out_x.resize(out_size);
+  out_y.resize(out_size);
+  EXPECT_EQ(out_x, true_result_x);
+  EXPECT_EQ(out_y, true_result_y);
 }
 
 TEST(leontev_n_graham_seq, test_20_points_square) {
   constexpr size_t kCount = 20;
 
   // Create data
-  std::vector<float> in_X = GenVec(kCount);
-  std::vector<float> in_Y = GenVec(kCount);
-  in_X[0] = -10.0f;
-  in_Y[0] = -10.0f;
-  in_X[1] = 10.0f;
-  in_Y[1] = -10.0f;
-  in_X[2] = 10.0f;
-  in_Y[2] = 10.0f;
-  in_X[3] = -10.0f;
-  in_Y[3] = 10.0f;
-  std::vector<float> out_X(kCount, 0.0f);
-  std::vector<float> out_Y(kCount, 0.0f);
+  std::vector<float> in_x = GenVec(kCount);
+  std::vector<float> in_y = GenVec(kCount);
+  in_x[0] = -10.0F;
+  in_y[0] = -10.0F;
+  in_x[1] = 10.0F;
+  in_y[1] = -10.0F;
+  in_x[2] = 10.0F;
+  in_y[2] = 10.0F;
+  in_x[3] = -10.0F;
+  in_y[3] = 10.0F;
+  std::vector<float> out_x(kCount, 0.0F);
+  std::vector<float> out_y(kCount, 0.0F);
   int out_size = 0;
-  std::vector<float> true_result_X = {-10.0f, 10.0f, 10.0f, -10.0f};
-  std::vector<float> true_result_Y = {-10.0f, -10.0f, 10.0f, 10.0f};
+  std::vector<float> true_result_x = {-10.0F, 10.0F, 10.0F, -10.0F};
+  std::vector<float> true_result_y = {-10.0F, -10.0F, 10.0F, 10.0F};
 
   // Create task_data
   auto task_data_seq = std::make_shared<ppc::core::TaskData>();
-  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in_X.data()));
-  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in_Y.data()));
-  task_data_seq->inputs_count.emplace_back(in_X.size());
-  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_X.data()));
-  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_Y.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in_x.data()));
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in_y.data()));
+  task_data_seq->inputs_count.emplace_back(in_x.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_x.data()));
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_y.data()));
   task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(&out_size));
-  task_data_seq->outputs_count.emplace_back(out_X.size());
+  task_data_seq->outputs_count.emplace_back(out_x.size());
 
   // Create Task
   leontev_n_graham_seq::GrahamSeq test_task_sequential(task_data_seq);
@@ -95,8 +92,8 @@ TEST(leontev_n_graham_seq, test_20_points_square) {
   test_task_sequential.PreProcessing();
   test_task_sequential.Run();
   test_task_sequential.PostProcessing();
-  out_X.resize(out_size);
-  out_Y.resize(out_size);
-  EXPECT_EQ(out_X, true_result_X);
-  EXPECT_EQ(out_Y, true_result_Y);
+  out_x.resize(out_size);
+  out_y.resize(out_size);
+  EXPECT_EQ(out_x, true_result_x);
+  EXPECT_EQ(out_y, true_result_y);
 }
--- tasks/seq/leontev_n_graham/src/ops_seq.cpp
+++ tasks/seq/leontev_n_graham/src/ops_seq.cpp
@@ -2,7 +2,9 @@
 
 #include <cmath>
 #include <cstddef>
+#include <ranges>
 #include <vector>
+#include <utility>
 
 bool leontev_n_graham_seq::GrahamSeq::PreProcessingImpl() {
   // Init value for input and output
@@ -23,43 +25,55 @@ bool leontev_n_graham_seq::GrahamSeq::ValidationImpl() {
   return task_data->inputs.size() == 2 && task_data->outputs_count[0] <= task_data->inputs_count[0];
 }
 
-std::pair<float, float> leontev_n_graham_seq::GrahamSeq::minus(std::pair<float, float> a, std::pair<float, float> b) {
+std::pair<float, float> leontev_n_graham_seq::GrahamSeq::Minus(std::pair<float, float> a, std::pair<float, float> b) {
   return {a.first - b.first, a.second - b.second};
 }
 
-float leontev_n_graham_seq::GrahamSeq::mul(std::pair<float, float> a, std::pair<float, float> b) {
-  return a.first * b.second - b.first * a.second;
+float leontev_n_graham_seq::GrahamSeq::Mul(std::pair<float, float> a, std::pair<float, float> b) {
+  return (a.first * b.second) - (b.first * a.second);
 }
 
 bool leontev_n_graham_seq::GrahamSeq::RunImpl() {
-  int amount_of_points = input_X_.size();
-  typedef std::pair<float, float> point;
+  size_t amount_of_points = input_X_.size();
+  using point = std::pair<float, float>;
   std::vector<point> points(amount_of_points);
   for (int i = 0; i < amount_of_points; i++) {
     points[i] = point(input_X_[i], input_Y_[i]);
   }
   point p0 = points[0];
-  for (point p : points)
-    if (p.first < p0.first || (p.first == p0.first && p.second < p0.second)) p0 = p;
+  for (point p : points) {
+    if (p.first < p0.first || (p.first == p0.first && p.second < p0.second)) {
+      p0 = p;
+    }
+  }
 
   // sort by polar angle
-  std::sort(points.begin(), points.end(), [&](point a, point b) { return mul(minus(a, p0), minus(b, p0)) > 0.0f; });
+  std::ranges::sort(points, [&](point a, point b) {
+    if (a == p0) {
+      return Mul((point(1.0F, 0.0F)), Minus(b, p0)) > 0.0F;
+    }
+    if (b == p0) {
+      return Mul(Minus(a, p0), point(1.0F, 0.0F)) > 0.0F;
+    }
+    return Mul(Minus(a, p0), Minus(b, p0)) > 0.0F;
+  });
 
   std::vector<point> hull;
   for (point p : points) {
     while (hull.size() >= 2) {
-      point new_vector = minus(p, hull.back());
-      point last_vector = minus(hull.back(), hull[hull.size() - 2]);
-      if (mul(new_vector, last_vector) >= 0.0f)
+      point new_vector = Minus(p, hull.back());
+      point last_vector = Minus(hull.back(), hull[hull.size() - 2]);
+      if (Mul(new_vector, last_vector) >= 0.0F) {
         hull.pop_back();
-      else
+      } else {
         break;
+      }
     }
     hull.push_back(p);
   }
   output_X_.resize(hull.size());
   output_Y_.resize(hull.size());
-  for (int i = 0; i < hull.size(); i++) {
+  for (size_t i = 0; i < hull.size(); i++) {
     output_X_[i] = hull[i].first;
     output_Y_[i] = hull[i].second;
   }
@@ -67,7 +81,7 @@ bool leontev_n_graham_seq::GrahamSeq::RunImpl() {
 }
 
 bool leontev_n_graham_seq::GrahamSeq::PostProcessingImpl() {
-  reinterpret_cast<int*>(task_data->outputs[2])[0] = static_cast<int>(output_X_.size());
+  reinterpret_cast<int *>(task_data->outputs[2])[0] = static_cast<int>(output_X_.size());
   for (size_t i = 0; i < output_X_.size(); i++) {
     reinterpret_cast<float *>(task_data->outputs[0])[i] = output_X_[i];
     reinterpret_cast<float *>(task_data->outputs[1])[i] = output_Y_[i];
