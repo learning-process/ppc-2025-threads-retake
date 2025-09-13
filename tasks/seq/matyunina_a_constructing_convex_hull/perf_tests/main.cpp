c5e500090023e37dc0ea001f118c51cf9d933535
erty1909 <ERTY-00@yandex.ru>
2025-09-14 01:02:41 +0300

    fix clang-tidy

--- tasks/seq/matyunina_a_constructing_convex_hull/perf_tests/main.cpp
+++ tasks/seq/matyunina_a_constructing_convex_hull/perf_tests/main.cpp
@@ -41,7 +41,7 @@ TEST(matyunina_a_constructing_convex_hull_seq, test_pipeline_run) {
   perf_analyzer->PipelineRun(perf_attr, perf_results);
   ppc::core::Perf::PrintPerfStatistic(perf_results);
 
-  auto* point_array = reinterpret_cast<matyunina_a_constructing_convex_hull_seq::Point *>(task_data->outputs[0]);
+  +  auto *point_array = reinterpret_cast<matyunina_a_constructing_convex_hull_seq::Point *>(task_data->outputs[0]);
   std::vector<matyunina_a_constructing_convex_hull_seq::Point> points(point_array,
                                                                       point_array + task_data->outputs_count[0]);
 
@@ -86,7 +86,7 @@ TEST(matyunina_a_constructing_convex_hull_seq, test_task_run) {
   perf_analyzer->TaskRun(perf_attr, perf_results);
   ppc::core::Perf::PrintPerfStatistic(perf_results);
 
-  auto* point_array = reinterpret_cast<matyunina_a_constructing_convex_hull_seq::Point *>(task_data->outputs[0]);
+  +  auto *point_array = reinterpret_cast<matyunina_a_constructing_convex_hull_seq::Point *>(task_data->outputs[0]);
   std::vector<matyunina_a_constructing_convex_hull_seq::Point> points(point_array,
                                                                       point_array + task_data->outputs_count[0]);
 
