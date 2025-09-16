fd67ff71ec49fa665041e0445d7f5a4dc981cd7f
pipidaster2004 <chebureknabite@gmail.com>
2025-09-16 16:33:29 +0300

    3

--- tasks/stl/khokhlov_a_shell_stl/src/ops_stl.cpp
+++ tasks/stl/khokhlov_a_shell_stl/src/ops_stl.cpp
@@ -28,7 +28,7 @@ bool khokhlov_a_shell_stl::ShellStl::PostProcessingImpl() {
   return true;
 }
 
-void khokhlov_a_shell_stl::ShellStl::ShellSortChunk(std::vector<int>& vec, int start, int end, int interval) {
+static void khokhlov_a_shell_stl::ShellStl::ShellSortChunk(std::vector<int>& vec, int start, int end, int interval) {
   for (int i = start + interval; i < end; ++i) {
     int tmp = vec[i];
     int j = i;
