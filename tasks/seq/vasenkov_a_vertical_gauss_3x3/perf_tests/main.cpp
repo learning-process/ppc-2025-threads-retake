514ed5985ba660c9d1fb286c8e52d0840cc1a11a
Andrey <andrej.500@mail.ru>
2025-09-18 21:20:38 +0300

    fix: ci

--- tasks/seq/vasenkov_a_vertical_gauss_3x3/perf_tests/main.cpp
+++ tasks/seq/vasenkov_a_vertical_gauss_3x3/perf_tests/main.cpp
@@ -39,6 +39,7 @@ std::vector<uint8_t> generate_performance_test_image(int width, int height) {
         image[idx + 1] = 255;
         image[idx + 2] = 255;
       }
+      
       if (x == width / 2) {
         image[idx] = 0;
         image[idx + 1] = 255;
