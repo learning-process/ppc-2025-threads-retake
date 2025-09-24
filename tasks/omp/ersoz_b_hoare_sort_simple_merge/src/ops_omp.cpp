a6466a4e8afa2591f529a998262a5cb93861009c
Berke Ersoz <145029854+berkeerenersoz@users.noreply.github.com>
2025-09-24 22:54:10 +0300

    fix: address minor issues and improve code quality

--- tasks/omp/ersoz_b_hoare_sort_simple_merge/src/ops_omp.cpp
+++ tasks/omp/ersoz_b_hoare_sort_simple_merge/src/ops_omp.cpp
@@ -99,13 +99,9 @@ bool HoareSortSimpleMergeOpenMP::RunImpl() {
 #pragma omp parallel sections default(none) shared(mid, n, input_)
   {
 #pragma omp section
-    {
-      QuickSortHoare(input_, 0, static_cast<long long>(mid) - 1);
-    }
+    { QuickSortHoare(input_, 0, static_cast<long long>(mid) - 1); }
 #pragma omp section
-    {
-      QuickSortHoare(input_, static_cast<long long>(mid), static_cast<long long>(n) - 1);
-    }
+    { QuickSortHoare(input_, static_cast<long long>(mid), static_cast<long long>(n) - 1); }
   }
   MergeTwo(input_, Segment{.begin = 0, .end = mid}, Segment{.begin = mid, .end = n}, output_);
   return true;
