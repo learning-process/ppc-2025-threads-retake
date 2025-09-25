813bcb47de4ac085244d8b43d38e42fb5bd56b34
Ev1ven <elvin200204@icloud.com>
2025-09-25 23:10:48 +0300

    clang-tidy

--- tasks/seq/veliev_e_montecarlo/src/ops_seq.cpp
+++ tasks/seq/veliev_e_montecarlo/src/ops_seq.cpp
@@ -16,7 +16,7 @@ bool veliev_e_monte_carlo_seq::VelievEMonteCarloSeq::PreProcessingImpl() {
   Int1_[1] = reinterpret_cast<double *>(task_data->inputs[0])[1];
   Int2_[0] = reinterpret_cast<double *>(task_data->inputs[1])[0];
   Int2_[1] = reinterpret_cast<double *>(task_data->inputs[1])[1];
-  function_ = reinterpret_cast<func>(task_data->inputs[2]);
+  function_ = reinterpret_cast<Func>(task_data->inputs[2]);
 
   N_ = *reinterpret_cast<int *>(task_data->inputs[3]);
   res_ = 0.0;
@@ -31,15 +31,15 @@ bool veliev_e_monte_carlo_seq::VelievEMonteCarloSeq::RunImpl() {
   double h1 = (Int1_[1] - Int1_[0]) / N_;
   double h2 = (Int2_[1] - Int2_[0]) / N_;
 
-  int i;
-  int j;
-  for (j = 0; j < N; ++j) {
+  int i{};
+  int j{};
+  for (j = 0; j < N_; ++j) {
     double y = Int2_[0] + (h2 * j);
-    for (i = 0; i < N; ++i) {
-      res_ += function_(Int1_[0] + (h1 * i), y_);
+    for (i = 0; i < N_; ++i) {
+      res_ += function_(Int1_[0] + (h1 * i), y);
     }
   }
-  res_ *= h1_ * h2_;
+  res_ *= h1 * h2;
 
   return true;
 }
