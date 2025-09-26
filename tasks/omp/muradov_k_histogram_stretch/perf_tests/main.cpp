79603b80d7292690d9986220b6a8887b754fd115
Kamal Muradov <muradoffk@gmail.com>
2025-09-26 09:32:55 +0300

    Reduce number of runs for histogram stretching to improve performance in CI

--- tasks/omp/muradov_k_histogram_stretch/perf_tests/main.cpp
+++ tasks/omp/muradov_k_histogram_stretch/perf_tests/main.cpp
@@ -42,7 +42,7 @@ TEST(muradov_k_histogram_stretch_omp, test_pipeline_run) {
   auto task = std::make_shared<muradov_k_histogram_stretch_omp::HistogramStretchOpenMP>(td);
 
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
-  perf_attr->num_running = 30000;
+  perf_attr->num_running = 10000;
   const auto t0 = std::chrono::high_resolution_clock::now();
   perf_attr->current_timer = [&]() -> double {
     auto now = std::chrono::high_resolution_clock::now();
@@ -80,7 +80,7 @@ TEST(muradov_k_histogram_stretch_omp, test_task_run) {
   auto task = std::make_shared<muradov_k_histogram_stretch_omp::HistogramStretchOpenMP>(td);
 
   auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
-  perf_attr->num_running = 30000;
+  perf_attr->num_running = 10000;
   const auto t0 = std::chrono::high_resolution_clock::now();
   perf_attr->current_timer = [&]() -> double {
     auto now = std::chrono::high_resolution_clock::now();
