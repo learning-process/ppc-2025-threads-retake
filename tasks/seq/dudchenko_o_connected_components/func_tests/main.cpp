d33aa3ed86db901248247820fc9a974bcdf6bd63
Uzzika <dudchenkoleska@gmail.com>
2025-09-27 20:02:25 +0300

    fix clang 7

--- tasks/seq/dudchenko_o_connected_components/func_tests/main.cpp
+++ tasks/seq/dudchenko_o_connected_components/func_tests/main.cpp
@@ -14,53 +14,53 @@
 #include "core/util/include/util.hpp"
 #include "seq/dudchenko_o_connected_components/include/ops_seq.hpp"
 
-TEST(dudchenko_o_connected_components, test_small_image) {
-  int width = 3;
-  int height = 3;
-  std::vector<int> image_data = {1, 0, 1, 0, 1, 0, 1, 0, 1};
+TEST(dudchenko_o_connected_components_seq, test_small_image) {
+    int width = 3;
+    int height = 3;
+    std::vector<int> image_data = {1, 0, 1, 0, 1, 0, 1, 0, 1};
+    
+    std::vector<int> input_data;
+    input_data.push_back(width);
+    input_data.push_back(height);
+    input_data.insert(input_data.end(), image_data.begin(), image_data.end());
+    
+    std::vector<int> output_data(width * height);
 
-  std::vector<int> input_data;
-  input_data.push_back(width);
-  input_data.push_back(height);
-  input_data.insert(input_data.end(), image_data.begin(), image_data.end());
+    auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+    task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_data.data()));
+    task_data_seq->inputs_count.emplace_back(input_data.size());
+    task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(output_data.data()));
+    task_data_seq->outputs_count.emplace_back(output_data.size());
 
-  std::vector<int> output_data(width * height);
+    dudchenko_o_connected_components::TestTaskSequential test_task_sequential(task_data_seq);
+    ASSERT_EQ(test_task_sequential.Validation(), true);
+    test_task_sequential.PreProcessing();
+    test_task_sequential.Run();
+    test_task_sequential.PostProcessing();
 
-  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
-  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_data.data()));
-  task_data_seq->inputs_count.emplace_back(input_data.size());
-  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(output_data.data()));
-  task_data_seq->outputs_count.emplace_back(output_data.size());
+    EXPECT_NE(output_data[0], 0);
+    EXPECT_EQ(output_data[1], 0);
+    EXPECT_NE(output_data[2], 0);
+    EXPECT_EQ(output_data[3], 0);
+    EXPECT_NE(output_data[4], 0);
+    EXPECT_EQ(output_data[5], 0);
+    EXPECT_NE(output_data[6], 0);
+    EXPECT_EQ(output_data[7], 0);
+    EXPECT_NE(output_data[8], 0);
 
-  dudchenko_o_connected_components::TestTaskSequential test_task_sequential(task_data_seq);
-  ASSERT_EQ(test_task_sequential.Validation(), true);
-  test_task_sequential.PreProcessing();
-  test_task_sequential.Run();
-  test_task_sequential.PostProcessing();
-
-  EXPECT_NE(output_data[0], 0);
-  EXPECT_EQ(output_data[1], 0);
-  EXPECT_NE(output_data[2], 0);
-  EXPECT_EQ(output_data[3], 0);
-  EXPECT_NE(output_data[4], 0);
-  EXPECT_EQ(output_data[5], 0);
-  EXPECT_NE(output_data[6], 0);
-  EXPECT_EQ(output_data[7], 0);
-  EXPECT_NE(output_data[8], 0);
-
-  EXPECT_NE(output_data[0], output_data[2]);
-  EXPECT_NE(output_data[0], output_data[4]);
-  EXPECT_NE(output_data[0], output_data[6]);
-  EXPECT_NE(output_data[0], output_data[8]);
-  EXPECT_NE(output_data[2], output_data[4]);
-  EXPECT_NE(output_data[2], output_data[6]);
-  EXPECT_NE(output_data[2], output_data[8]);
-  EXPECT_NE(output_data[4], output_data[6]);
-  EXPECT_NE(output_data[4], output_data[8]);
-  EXPECT_NE(output_data[6], output_data[8]);
+    EXPECT_NE(output_data[0], output_data[2]);
+    EXPECT_NE(output_data[0], output_data[4]);
+    EXPECT_NE(output_data[0], output_data[6]);
+    EXPECT_NE(output_data[0], output_data[8]);
+    EXPECT_NE(output_data[2], output_data[4]);
+    EXPECT_NE(output_data[2], output_data[6]);
+    EXPECT_NE(output_data[2], output_data[8]);
+    EXPECT_NE(output_data[4], output_data[6]);
+    EXPECT_NE(output_data[4], output_data[8]);
+    EXPECT_NE(output_data[6], output_data[8]);
 }
 
-TEST(dudchenko_o_connected_components, test_single_component) {
+TEST(dudchenko_o_connected_components_seq, test_single_component) {
   int width = 4;
   int height = 4;
   std::vector<int> image_data(width * height, 1);
@@ -91,7 +91,7 @@ TEST(dudchenko_o_connected_components, test_single_component) {
   }
 }
 
-TEST(dudchenko_o_connected_components, test_no_components) {
+TEST(dudchenko_o_connected_components_seq, test_no_components) {
   int width = 4;
   int height = 4;
   std::vector<int> image_data(width * height, 0);
@@ -120,10 +120,11 @@ TEST(dudchenko_o_connected_components, test_no_components) {
   }
 }
 
-TEST(dudchenko_o_connected_components, test_two_separate_components) {
+TEST(dudchenko_o_connected_components_seq, test_two_separate_components) {
   int width = 5;
   int height = 5;
-  std::vector<int> image_data = {1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1};
+  std::vector<int> image_data = {1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+                                 0, 0, 0, 1, 1, 0, 0, 0, 1, 1};
 
   std::vector<int> input_data;
   input_data.push_back(width);
@@ -144,19 +145,17 @@ TEST(dudchenko_o_connected_components, test_two_separate_components) {
   test_task_sequential.Run();
   test_task_sequential.PostProcessing();
 
-  EXPECT_NE(output_data[0], 0);
-  EXPECT_EQ(output_data[0], output_data[1]);
-  EXPECT_EQ(output_data[0], output_data[5]);
-  EXPECT_EQ(output_data[0], output_data[6]);
+  int comp1 = output_data[0];
+  int comp2 = output_data[18];
 
-  EXPECT_NE(output_data[18], 0);
-  EXPECT_EQ(output_data[18], output_data[19]);
-  EXPECT_EQ(output_data[18], output_data[23]);
-  EXPECT_EQ(output_data[18], output_data[24]);
+  EXPECT_NE(comp1, 0);
+  EXPECT_NE(comp2, 0);
+  EXPECT_NE(comp1, comp2);
 
-  EXPECT_NE(output_data[0], output_data[18]);
-  EXPECT_EQ(output_data[2], 0);
-  EXPECT_EQ(output_data[7], 0);
-  EXPECT_EQ(output_data[12], 0);
-  EXPECT_EQ(output_data[17], 0);
+  EXPECT_EQ(output_data[1], comp1);
+  EXPECT_EQ(output_data[5], comp1);
+  EXPECT_EQ(output_data[6], comp1);
+  EXPECT_EQ(output_data[19], comp2);
+  EXPECT_EQ(output_data[23], comp2);
+  EXPECT_EQ(output_data[24], comp2);
 }
