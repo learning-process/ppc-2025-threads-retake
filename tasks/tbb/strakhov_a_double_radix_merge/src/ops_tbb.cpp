3a44a4a318e6e5b2d079979c239bbb1cea99f929
Astra-chan-desu <lyosha5438@gmail.com>
2025-09-27 07:20:05 +0300

    fixedbrackets

--- tasks/tbb/strakhov_a_double_radix_merge/src/ops_tbb.cpp
+++ tasks/tbb/strakhov_a_double_radix_merge/src/ops_tbb.cpp
@@ -1,4 +1,4 @@
-#include <oneapi/tbb/blocked_range.h>
+#include <oneapi/tbb/oneapi::tbb::blocked_range.h>
 #include <oneapi/tbb/parallel_for.h>
 
 #include <algorithm>
@@ -55,38 +55,43 @@ inline void strakhov_a_double_radix_merge_omp::DoubleRadixMergeOmp::MergeSorted(
 
 inline void strakhov_a_double_radix_merge_omp::DoubleRadixMergeOmp::FloatToNormalized(std::vector<uint64_t> &input,
                                                                                       unsigned size) {
-oneapi::tbb::parallel_for(blocked_range<size_t>(0, size), [&](const blocked_range<size_t>& r){
-    for (size_t i = r.begin(); i != r.end(); ++i) {
-      uint64_t bits = 0;
-      std::memcpy(&bits, &input_[i], sizeof(double));
-      if ((bits >> 63) != 0ULL) {
-        input[i] = ~bits;
-      } else {
-        input[i] = bits ^ 0x8000000000000000ULL;
-      }
-    }
+  oneapi::tbb::parallel_for(oneapi::tbb::blocked_range<size_t>(0, size),
+                            [&](const oneapi::tbb::blocked_range<size_t> &r) {
+                              for (size_t i = r.begin(); i != r.end(); ++i) {
+                                uint64_t bits = 0;
+                                std::memcpy(&bits, &input_[i], sizeof(double));
+                                if ((bits >> 63) != 0ULL) {
+                                  input[i] = ~bits;
+                                } else {
+                                  input[i] = bits ^ 0x8000000000000000ULL;
+                                }
+                              }
+                            });
 }
 inline void strakhov_a_double_radix_merge_omp::DoubleRadixMergeOmp::NormalizedToFloat(std::vector<uint64_t> &input,
                                                                                       unsigned size) {
-    // uint64 to float
-oneapi::tbb::parallel_for(blocked_range<size_t>(0, size), [&](const blocked_range<size_t>& r){
-      for (size_t i = r.begin(); i != r.end(); ++i) {
-        uint64_t k = input[i];
-        if ((k >> 63) != 0ULL) {
-          k ^= 0x8000000000000000ULL;
-        } else {
-          k = ~k;
-        }
-        std::memcpy(&output_[i], &k, sizeof(double));
-      }
+  // uint64 to float
+  oneapi::tbb::parallel_for(oneapi::tbb::blocked_range<size_t>(0, size),
+                            [&](const oneapi::tbb::blocked_range<size_t> &r) {
+                              for (size_t i = r.begin(); i != r.end(); ++i) {
+                                uint64_t k = input[i];
+                                if ((k >> 63) != 0ULL) {
+                                  k ^= 0x8000000000000000ULL;
+                                } else {
+                                  k = ~k;
+                                }
+                                std::memcpy(&output_[i], &k, sizeof(double));
+                              }
+                            });
 }
 
 inline void strakhov_a_double_radix_merge_omp::DoubleRadixMergeOmp::RadixGrandSort(size_t n,
                                                                                    std::vector<uint64_t> &input,
                                                                                    size_t chunk_size) {
-      const size_t num_chunks = (n + chunk_size - 1) / chunk_size;
-      const int type_length = sizeof(double) * 8;
-oneapi::tbb::parallel_for(blocked_range<size_t>(0, num_chunks), [&](const blocked_range<size_t>& rr){
+  const size_t num_chunks = (n + chunk_size - 1) / chunk_size;
+  const int type_length = sizeof(double) * 8;
+  oneapi::tbb::parallel_for(
+      oneapi::tbb::blocked_range<size_t>(0, num_chunks), [&](const oneapi::tbb::blocked_range<size_t> &rr) {
         for (size_t t = rr.begin(); t != rr.end(); ++t) {
           const size_t start = (size_t)t * chunk_size;
           const size_t end = std::min(start + chunk_size, n);
@@ -118,61 +123,68 @@ oneapi::tbb::parallel_for(blocked_range<size_t>(0, num_chunks), [&](const blocke
             }
           }
         }
+      });
 }
 
 bool strakhov_a_double_radix_merge_omp::DoubleRadixMergeOmp::ValidationImpl() {
-        // Check equality of counts elements
-        return task_data->inputs_count[0] == task_data->outputs_count[0];
+  // Check equality of counts elements
+  return task_data->inputs_count[0] == task_data->outputs_count[0];
 }
 
 bool strakhov_a_double_radix_merge_omp::DoubleRadixMergeOmp::RunImpl() {
-        unsigned int size = task_data->inputs_count[0];
-        if (size == 0) {
-          return true;
-        }
+  unsigned int size = task_data->inputs_count[0];
+  if (size == 0) {
+    return true;
+  }
 
-        std::vector<uint64_t> temp_vector(size);
-        FloatToNormalized(temp_vector, size);
-        // radix sort
-        auto n = static_cast<size_t>(size);
-        const size_t chunk_size = 1U << 14;
-        RadixGrandSort(n, temp_vector, chunk_size);
-        // simple merge
-        std::vector<uint64_t> shadow_vector(size);
-        uint64_t *temp_ptr = temp_vector.data();
-        uint64_t *shadow_ptr = shadow_vector.data();
+  std::vector<uint64_t> temp_vector(size);
+  FloatToNormalized(temp_vector, size);
+  // radix sort
+  auto n = static_cast<size_t>(size);
+  const size_t chunk_size = 1U << 14;
+  RadixGrandSort(n, temp_vector, chunk_size);
+  // simple merge
+  std::vector<uint64_t> shadow_vector(size);
+  uint64_t *temp_ptr = temp_vector.data();
+  uint64_t *shadow_ptr = shadow_vector.data();
 
-        for (size_t run = chunk_size; run < n; run <<= 1) {
-oneapi::tbb::parallel_for(blocked_range<size_t>(0, n, (run << 1)), [&](const blocked_range<size_t>& r){
-            for (size_t base = r.begin(); base < r.end(); base += (run << 1)) {
-              auto left_start = (size_t)base;
-              size_t left_end = std::min(left_start + run, n);
-              size_t right_start = left_end;
-              size_t right_end = std::min(left_end + run, n);
-              if (right_start < right_end) {
-                MergeSorted(temp_ptr, Range{.start = left_start, .end = left_end},
-                            Range{.start = right_start, .end = right_end}, shadow_ptr, left_start);
-              } else {
-                for (size_t i = left_start; i < left_end; i++) {
-                  shadow_ptr[i] = temp_ptr[i];
-                }
-              }
-            }
-            std::swap(temp_ptr, shadow_ptr);
+  for (size_t run = chunk_size; run < n; run <<= 1) {
+    oneapi::tbb::parallel_for(oneapi::tbb::blocked_range<size_t>(0, n, (run << 1)),
+                              [&](const oneapi::tbb::blocked_range<size_t> &r) {
+                                for (size_t base = r.begin(); base < r.end(); base += (run << 1)) {
+                                  auto left_start = (size_t)base;
+                                  size_t left_end = std::min(left_start + run, n);
+                                  size_t right_start = left_end;
+                                  size_t right_end = std::min(left_end + run, n);
+                                  if (right_start < right_end) {
+                                    MergeSorted(temp_ptr, Range{.start = left_start, .end = left_end},
+                                                Range{.start = right_start, .end = right_end}, shadow_ptr, left_start);
+                                  } else {
+                                    for (size_t i = left_start; i < left_end; i++) {
+                                      shadow_ptr[i] = temp_ptr[i];
+                                    }
+                                  }
+                                }
+                              });
+    std::swap(temp_ptr, shadow_ptr);
   }
   if (temp_ptr != temp_vector.data()) {
-            oneapi::tbb::parallel_for(blocked_range<size_t>(0, n), [&](const blocked_range<size_t> &r) {
-              std::memcpy(temp_vector.data() + r.begin(), temp_ptr + r.begin(),
-                          (r.end() - r.begin()) * sizeof(uint64_t));
-            });
-            NormalizedToFloat(temp_vector, size);
-            return true;
-}
+    oneapi::tbb::parallel_for(
+        oneapi::tbb::blocked_range<size_t>(0, n), [&](const oneapi::tbb::blocked_range<size_t> &r) {
+          std::memcpy(temp_vector.data() + r.begin(), temp_ptr + r.begin(), (r.end() - r.begin()) * sizeof(uint64_t));
+        });
+    NormalizedToFloat(temp_vector, size);
+    return true;
+  }
 
-bool strakhov_a_double_radix_merge_omp::DoubleRadixMergeOmp::PostProcessingImpl() {
-oneapi::tbb::parallel_for(blocked_range<size_t>(0, size), [&](const blocked_range<size_t>& r){
-              for (size_t i = r.begin(); i != r.end(); ++i) {
-                reinterpret_cast<double *>(task_data->outputs[0])[i] = output_[i];
-              }
-              return true;
-}
+  bool strakhov_a_double_radix_merge_omp::DoubleRadixMergeOmp::PostProcessingImpl() {
+    unsigned long size = output_.size();
+
+    oneapi::tbb::parallel_for(oneapi::tbb::blocked_range<size_t>(0, size),
+                              [&](const oneapi::tbb::blocked_range<size_t> &r) {
+                                for (size_t i = r.begin(); i != r.end(); ++i) {
+                                  reinterpret_cast<double *>(task_data->outputs[0])[i] = output_[i];
+                                }
+                              });
+    return true;
+  }
