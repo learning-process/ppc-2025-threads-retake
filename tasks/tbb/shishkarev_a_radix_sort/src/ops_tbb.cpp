2423d97b1857e1dc0c8b5d8ccf9805bf1afa5c77
shishkarev <and01072004@gmail.com>
2025-09-28 19:52:35 +0300

    fix clang

--- tasks/tbb/shishkarev_a_radix_sort/src/ops_tbb.cpp
+++ tasks/tbb/shishkarev_a_radix_sort/src/ops_tbb.cpp
@@ -2,9 +2,10 @@
 
 #include <tbb/blocked_range.h>
 #include <tbb/parallel_for.h>
-#include <tbb/parallel_invoke.h>
 #include <tbb/parallel_reduce.h>
+#include <tbb/parallel_invoke.h>
 #include <tbb/spin_mutex.h>
+#include <tbb/task_arena.h>
 
 #include <algorithm>
 #include <cmath>
