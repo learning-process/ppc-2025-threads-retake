e2efc0158c45bdc251b249b16f35703cd6dfa688
ChernovaNatalya <natali-4ch@yandex.ru>
2025-09-30 00:03:12 +0300

    tbb 2

--- tasks/tbb/chernova_n_cannon_matrix_mul/include/ops_tbb.hpp
+++ tasks/tbb/chernova_n_cannon_matrix_mul/include/ops_tbb.hpp
@@ -5,26 +5,25 @@
 
 #include "core/task/include/task.hpp"
 
-namespace chernova_n_cannon_matrix_mul_tbb
-{
+namespace chernova_n_cannon_matrix_mul_tbb {
 
-  class TestTaskTBB : public ppc::core::Task
-  {
-  public:
-    explicit TestTaskTBB(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
-    bool PreProcessingImpl() override;
-    bool ValidationImpl() override;
-    bool RunImpl() override;
-    bool PostProcessingImpl() override;
+class TestTaskTBB : public ppc::core::Task {
+ public:
+  explicit TestTaskTBB(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
+  bool PreProcessingImpl() override;
+  bool ValidationImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
 
-  private:
-    std::vector<double> matrixA_;
-    std::vector<double> matrixB_;
-    std::vector<double> res_;
-    int n_ = 0;
-  };
+ private:
+  std::vector<double> matrixA_;
+  std::vector<double> matrixB_;
+  std::vector<double> res_;
+  int n_ = 0;
+};
 
-  std::vector<double> MultiplyMatrixTBB(const std::vector<double> &mat_a, const std::vector<double> &mat_b, int n);
-  std::vector<double> CannonMatrixMultiplicationTBB(const std::vector<double> &mat_a, const std::vector<double> &mat_b, int n);
+std::vector<double> MultiplyMatrixTBB(const std::vector<double> &mat_a, const std::vector<double> &mat_b, int n);
+std::vector<double> CannonMatrixMultiplicationTBB(const std::vector<double> &mat_a, const std::vector<double> &mat_b,
+                                                  int n);
 
-} // namespace chernova_n_cannon_matrix_mul_tbb
+}  // namespace chernova_n_cannon_matrix_mul_tbb
