d5c2d0277eb2625d92147706e22070961c2685ff
Ev1ven <elvin200204@icloud.com>
2025-09-29 20:10:36 +0300

    same fix as in tbb

--- tasks/all/veliev_e_montecarlo/src/ops_all.cpp
+++ tasks/all/veliev_e_montecarlo/src/ops_all.cpp
@@ -115,7 +115,8 @@ bool veliev_e_monte_carlo_all::VelievEMonteCarloAll::RunImpl() {
     double local_res_sum = local_res * (h1 * h2);
 
     double global_res = 0.0;
-    boost::mpi::reduce(world_, local_res_sum, global_res, std::plus<>(), 0);
+    boost::mpi::reduce(world_, local_res_sum, global_res,
+                       [](double a, double b) { return a + b; }, 0);
 
     if (rank == 0) {
       res_ = global_res;
