9a4eaf15e2d908d0de4b4f4f8409c4eb72d499d4
zdrt777 <apolyakov_2004@mail.ru>
2025-10-01 00:16:17 +0300

    finale

--- tasks/omp/polyakov_a_mult_complex_matrix_CRS/src/ops_omp.cpp
+++ tasks/omp/polyakov_a_mult_complex_matrix_CRS/src/ops_omp.cpp
@@ -72,10 +72,10 @@ bool polyakov_a_mult_complex_matrix_crs_omp::TestTaskOMP::RunImpl() {
 
 #pragma omp parallel
   {
-    std::vector<char> local_marked(c_cols_, 0);
+    std::vector<char> local_marked(c_cols_);
 #pragma omp for
     for (int r = 0; r < a_rows_int; r++) {
-      std::ranges::fill(local_marked.begin(), local_marked.end(), 0);
+      std::ranges::fill(local_marked, static_cast<char>(0));
       for (size_t i = a_->row_ptr[r]; i < a_->row_ptr[r + 1]; i++) {
         size_t k = a_->col_ind[i];
         for (size_t j = b_->row_ptr[k]; j < b_->row_ptr[k + 1]; j++) {
@@ -102,7 +102,7 @@ bool polyakov_a_mult_complex_matrix_crs_omp::TestTaskOMP::RunImpl() {
     std::vector<std::complex<double>> local_temp(c_cols_);
 #pragma omp for
     for (int r = 0; r < a_rows_int; r++) {
-      std::ranges::fill(local_temp.begin(), local_temp.end(), std::complex<double>(0.0));
+      std::ranges::fill(local_temp, std::complex<double>(0.0));
 
       // Умножение строки r матрицы A на B
       for (size_t i = a_->row_ptr[r]; i < a_->row_ptr[r + 1]; i++) {
